0f77a95861727f726b8ab27457a42f5647793bd0
nutlope <he55@drexel.edu>
2021-04-25 14:53:35 -0400

    refactor + added README

---
+++ README.md
@@ -0,0 +1,11 @@
+# JSON to FireStore
+
+A script that turns a JSON file (array of objects) into a FireStore collection.
+
+# Usage
+
+1. Download service_key from firebase and place it in the root directory
+2. Create a folder named "files" and put all JSON files there
+3. Specify the unique ID in L25 of uploader.js
+4. Run "npm install firebase firebase-admin"
+5. Run "node uploader.js"
--- uploader.js
+++ uploader.js
@@ -16,13 +16,10 @@ fs.readdir(directoryPath, function (err, files) {
   }
 
   files.forEach(function (file) {
-    console.log("File is, ", file);
     var lastDotIndex = file.lastIndexOf(".");
     var menu = require("./files/" + file);
 
     menu.forEach(function (obj) {
-      console.log(obj.id);
-      console.log(file.substring(0, lastDotIndex));
       firestore
         .collection(file.substring(0, lastDotIndex))
         .doc(obj.id)
